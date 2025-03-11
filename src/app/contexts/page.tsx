6caf8f276b427003ccc75675639948f50fa1a4f7
kamelCda <kamel.cda2024@gmail.com>
2025-03-11 16:28:19 +0100

    ajout use client

--- src/app/contexts/page.tsx
+++ src/app/contexts/page.tsx
@@ -1,18 +1,23 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 type UtilisateurContextType = {
   utilisateurId: string | null;
 };
 
-const UtilisateurContext = createContext<UtilisateurContextType>({ utilisateurId: null });
+const UtilisateurContext = createContext<UtilisateurContextType>({
+  utilisateurId: null,
+});
 
-export const UtilisateurProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const UtilisateurProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [utilisateurId, setUtilisateurId] = useState<string | null>(null);
 
   useEffect(() => {
     // Simulation de récupération de l'utilisateur connecté depuis une API
     const fetchUtilisateur = async () => {
-      const response = await fetch('/api/utilisateur');
+      const response = await fetch("/api/utilisateur");
       const data = await response.json();
       setUtilisateurId(data.utilisateur_id);
     };
